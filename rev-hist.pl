ed5c74c0f56efee137ee8f64381aa42e302e4825
rarlp137 <rarlp137@users.noreply.github.com>
2016-03-24 07:21:46 +0200

    minor corrs in intention 2 support the memoization

--- rev-hist.pl
+++ rev-hist.pl
@@ -1,27 +1,39 @@
 #! /usr/bin/env perl
-use strict; 
+
 # TODO: 
-#	make auth to unit state-vars
-#	clean docs, getopts, pass2DBX?
-#	rewrite according to Perl::Critic --brutal severity
-#	outpace pragmas
-#	strip into modules
-#	patch-semantics
+# - make auth to unit state-vars
+# - clean docs, getopts, pass2DBX?
+# - rewrite according to Perl::Critic --brutal severity
+# - outpace pragmas
+# - strip into modules
+# - patch-semantics
+
+use strict;
 use warnings;
 use diagnostics;
 use 5.20.00;
-use feature 'signatures'; no warnings qw(experimental::signatures);
-no warnings qw(experimental::autoderef);
+#use feature 'signatures'; # deprecated
+no warnings qw/experimental::autoderef/;
 use Getopt::Long;
+use Pod::Usage qw(pod2usage);
 #use Config::JSON;
-use Data::Dumper; # Perl core module
+# provides eDumper(), >human-readable than Dumper & w/ utf support
+use Data::Dumper::AutoEncode; 
+	$Data::Dumper::AutoEncode::ENCODING = 'utf8';
+	$Data::Dumper::Indent = 1; # turn off all pretty print
+	$Data::Dumper::Pair = "="; # specify hash key/value separator
+	$Data::Dumper::Sortkeys = 1;
+use Memoize::Storable; # for caching support
+#use Contextual::Return; # for managing context-sensitive retn's
+#use Hook::LexWrap; # for managing the pre- and post- subroutine wrappers
+
 #use Data::Dump qw(dump); # $ cpanm Data::Dump
 #use Data::DPath 'dpath';
 #use Data::Focus qw(focus);
 use Time::HiRes qw(time);
 #use Hash::Transform;
 #use Smart::Comments;
-use Params::Validate qw(:all); # for internal signature ctrl
+use Params::Validate qw(:all);
 
 use DateTime;
 use DateTime::Format::RFC3339;
@@ -30,12 +42,24 @@ use Mojo::JWT::Google;
 ## Crypt::OpenSSL::RSA # isok w/o, not necessary
 use Mojo::Collection 'c';
 use Mojo::UserAgent;
-use utf8;
+use utf8; # ::all
 binmode STDOUT, ":encoding(UTF-8)";
 
-## add the GetOpt's and usage
+# TODO: add the GetOpt's and usage
 
 ## init the globals
+
+state %counter;
+sub tickle_sub { # 4 debug purposes
+	my ($arg) = shift;
+	# Peek who's calling through the context:
+	my ($upkg, $ufile,	$uline,	$call_sub)	= caller 2;# who called...
+	my ($pkg, $file,	$line,	$sub_name)	= caller 1;# ...what?
+	# Increment the calls counter
+	$counter{$sub_name}{'from'}{$call_sub}{calls} ++;
+	return 0;
+}
+
 
 sub shave($hash, @fkeys) { # nice filter-lens-transform
 	my %shaved; # Data::Focus?
@@ -65,71 +89,110 @@ sub time2ut {  # add wantarray
 	return $dt->epoch;
 }
 
-# initiate OAuth2.0 session
-my $jwt = Mojo::JWT::Google->new( 
-	from_json => './secret.json',
-    issue_at  => time,
-    scopes    => c('https://www.googleapis.com/auth/drive.readonly'),
-    user_as   => 'some@email.com'
-);
+state ($ua, $auth_key, $call_header);
 
-my $token_request_url = q(https://www.googleapis.com/oauth2/v3/token);
-my $grant_type = 'urn:ietf:params:oauth:grant-type:jwt-bearer';
- 
-my $ua = Mojo::UserAgent->new;
-my $tok_res = $ua->post($token_request_url
-  => form => { grant_type => $grant_type, assertion => $jwt->encode } 
-  )->res;
-my $json = $tok_res->json;
-my $auth_key = $json->{token_type} . ' ' . $json->{access_token};
 
-# API caller proto
-sub call_api {
-	my %arg = validate( @_, { # Validate arguments
+# initiate the OAuth2.0 session
+sub init_session { 
+# TODO: expose for chain-continuation // done partially
+	my $jwt = Mojo::JWT::Google->new( 
+		from_json => './secret.json',
+		issue_at  => time,
+		scopes    => c('https://www.googleapis.com/auth/drive.readonly'),
+		user_as   => 
+			'some@email.com'
+	);
+
+	my $token_request_url = q(https://www.googleapis.com/oauth2/v3/token);
+	my $grant_type = 'urn:ietf:params:oauth:grant-type:jwt-bearer';
+
+	$ua = Mojo::UserAgent->new;
+	my $tok_res = $ua->post($token_request_url
+		=> form => { grant_type => $grant_type, assertion => $jwt->encode } 
+		)->res;
+	my $json = $tok_res->json or die "No authorization provided";
+	$auth_key = $json->{token_type} . ' ' . $json->{access_token};
+	$call_header =  {'Authorization' => $auth_key};
+}
+
+init_session();
+
+
+# Prepare the request string
+sub prepare_request {
+	my %arg = validate( @_, {
 		target	=> {type => HASHREF, optional => 1},
-		site 	=> { default => 'https://www.googleapis.com/drive/v3/' },
+		site 	=> { default => 'https://www.googleapis.com/drive' },
+		version	=> {type => SCALAR, optional => 1, # v2 and v3 both work fine
+			default => 'v3'},
 		api		=> {type => SCALAR},# What API are we using?
-		debug	=> {type => SCALAR, optional => 1, default => ''}, ## 'request', 'response' or 'time'
 		flags	=> {type => ARRAYREF|UNDEF, optional => 1}, # call properties
 		fields	=> {type => ARRAYREF|UNDEF, optional => 1}, # call response fields
-		auth_key=> {type => SCALAR, optional => 
-			  sub { return defined $auth_key}},
+		
+		debug	=> {type => SCALAR, optional => 1, default => ''},
 	});
-	print "\tRequest arguments: ", Dumper {%arg} if 
-		($arg{debug} eq 'request');
+	print "\tRequest arguments: ", Dumper {%arg} if ($arg{debug} eq 'request');
 	
-	my $url = $arg{site} . $arg{api} ;
-	my @flags = ();
-	if ($arg{flags}) {
+	# Construct the URL base 
+	my $url = $arg{site} .'/'. $arg{version} .'/'. $arg{api} ;
+	
+	# Append properties
+	my @flags = (); #  preventive init for later use in fields augmentation
+	if ($arg{flags}) { # ensure flags aren't empty
 		@flags = @{ $arg{flags}  } ;
 		$url .= '?' . join('&', @flags ) ;
 	}
+	my @fields= @{ $arg{fields} } ;
+	# Append fields
 	if ($arg{fields}) {
-		my @fields= @{ $arg{fields} } ;
-		$url .= ($arg{flags}?'&':'?') . 'fields='.join(',', @fields ) ;
+		$url .= ($arg{flags}?'&':'?') . # collate properties/fields separator
+			  'fields='.join(',', @fields ) ;
 	}
+	
 	print "GET request :\n\t$url\n" if ($arg{debug} eq 'request');
+	return $url; # TODO rewrite in Contextual::Return
+}
+
+sub cached_api_call {
+	...
+}
+
+sub actual_api_call {# consider to have plenty errors
+	my %arg = validate( @_, {
+		target	=> {type => HASHREF, optional => 1},
+		url		=> {type => SCALAR}, # mandatory
+		auth_key=> {type => SCALAR, optional => 
+			  # nasty trick to enforce the existing auth key to be required:
+			  sub { return defined $auth_key}}, # if no internal, passes 0
+		debug	=> {type => SCALAR, optional => 1, default => ''},
+	});
+	print "\tRequest arguments: ", Dumper {%arg} if ($arg{debug} eq 'request');
+	
+	# Check if we're supplied with some specific auth-key
 	if (defined $arg{auth_key}) { 
 		$auth_key = $arg{auth_key}; 
 		print "Using locally-proposed auth_key: $auth_key\n" if 
 			($arg{debug} eq 'response');
 	};
-	my $tdiff = 0; # onsite, non-integrating!
-	my $call_res;
+	my $tdiff = 0; # onsite, non-integrating!	
+	my $call_res = ();
 	if (defined $auth_key) {
 		my $time0 = time;
-		my $call_header =  {'Authorization' => $auth_key};
+			tickle_sub();
+		my $url = $arg{url};
+		#my $call_header =  {'Authorization' => $auth_key};
 		$call_res = $ua->get( $url  => $call_header)->res 
 			or die "Can't call $arg{api}-api!\n";
+			#print Dumper $call_res, "\n"x2;
 		if ($call_res->{error}) {
-			
-			# implement some error handling here
-			
+			# TODO: implement some error handling here
+			my $error = $call_res->{error};
+			print "\nError ", $error->{code}, ": ", $error->{message} || 0, "\n";
 		}
-		$tdiff = time - $time0;
+		$tdiff = time - $time0; # TODO: strip down this crap
 		print "Response: ", Dumper $call_res->json if 
 			($arg{debug} eq 'response');
-		print "Response took: $tdiff s\n" if 
+		print "Response took: ", sprintf("%03d",int($tdiff*1000))," msec\n" if 
 			($arg{debug} eq 'time');
 	} else {die "No authentification! \n"}
 	if (defined $arg{target}) {
@@ -138,19 +201,50 @@ sub call_api {
 	} else {
 		return $call_res->json;
 	};
+	#...
+}
+
+
+sub call_api {
+# wrapper
+# TODO:
+# - flatten to wrapper for memoization 
+# - try not to break the outer calls
+# - pledge to work with hashes
+# - fix the debug semantics 
+# 		D(&x): &h -> {&g'} => &h.&x -> {&g'.&x'}.&x
+# - log api calls
+# - implement API-call cache to minimize the actual API usage
+	my %arg = validate( @_, { # Validate arguments
+		target	=> {type => HASHREF, optional => 1},
+		auth_key=> {type => SCALAR, optional => 1},
+		request	=> {type => SCALAR}, # strict request string inquiry
+		allow_cached
+				=> {type => SCALAR, optional => 1, default => 1},
+		debug	=> {type => SCALAR, optional => 1, 
+			default => ''}, ## 'request', 'response' or 'time'
+	});
+	print "\tRequest arguments: ", Dumper {%arg} if ($arg{debug} eq 'request');
+
+	my $request = $arg{request};
+	my $call_res = actual_api_call(
+		url => $request,
+		#debug => 'request',
+		);
+	return $call_res;
 };
+
 
 
 
 # Fetch list of available files with id's to play w/ proto
 sub file_list_get {#Fetch list of available files with id's to play w/
-#L<https://developers.google.com/drive/v3/reference/files/list>
-#Resp: { "kind": "drive#fileList", "files": [  {  id  } , ... ]
 	my @fkeys = qw/id kind name trashed mimeType owners size md5Checksum 
 		createdTime modifiedTime headRevisionId/;
 	my @flags = qw/corpus=domain orderBy=folder/;
 	my %arg = validate( @_, {
 		target	=> {type => HASHREF, optional => 1},
+		#fields	=> {type => ARRAYREF, optional => 1},
 		fkeys	=> {type => ARRAYREF, optional => 1, default =>\@fkeys},
 		flags	=> {type => ARRAYREF, optional => 1, default =>\@flags}, 
 		shave	=> {type => SCALAR, optional => 1},
@@ -158,19 +252,25 @@ sub file_list_get {#Fetch list of available files with id's to play w/
 		});
 	
 	my @fields =qw/files/;
-	my $response = call_api(
-		api => 'files', 
-		flags => \@flags,
-		fields=> \@fields,
-		#debug => \$arg{debug}, # now has none, 4 validation concerns
+	
+	my $request_string = prepare_request(
+		api		=> 'files', 
+		flags 	=> \@flags,
+		fields	=> \@fields,
 	);
+	
+	my $response = call_api(
+		request => $request_string,
+		#debug => \$arg{debug},
+	);
+	
 	foreach my $file ( @{ $response->{files} } ) { 
 		my %fhash = %{ shave($file, @fkeys) } if (@fkeys);
 		update_fields(\%fhash, \&time2ut, qw/createdTime modifiedTime/);
 		print Dumper \%fhash if $arg{debug};
 	}
 	
-	
+	return $response;
 }
 
 
@@ -183,19 +283,22 @@ sub file_get {
 	my %arg = validate( @_, {
 		target	=> {type => HASHREF, optional => 1},
 		file_id	=> {type => SCALAR},
-		flags	=> {type => ARRAYREF, optional => 1}, ## need 2 rewrite
-		fields	=> {type => ARRAYREF, optional => 1},
+		fields	=> {type => ARRAYREF, optional => 1}, # rewrite?
 		fkeys	=> {type => ARRAYREF, optional => 1},
-		flags	=> {type => ARRAYREF, optional => 1}, 
 		shave	=> {type => SCALAR, optional => 1},
 		debug 	=> {type => SCALAR, optional => 1, default => undef},
 		});
 	my @fields = ($arg{fields}) ? $arg{fields} : @default_fields;
+	
+	my $request_string = prepare_request(
+		api		=> "files/$arg{file_id}/",
+		flags	=> undef,
+		fields	=> \@fields,
+		);
 	my $response = call_api(
-			api		=> "files/$arg{file_id}/",
-			flags	=> undef,
-			fields	=> \@fields,
-			);
+		request => $request_string,
+		debug => 'all'#\$arg{debug},
+		);
 	
 	my @f2upd = qw/createdTime modifiedTime/;
 	update_fields(
@@ -204,10 +307,11 @@ sub file_get {
 		fields	=> \@f2upd
 		);
 	print Dumper $response if $arg{debug};
-	
-	
+	return $response;
 }
 
+#file_get(file_id => $test_docyment,);
+#my $test_document = '' unless $glob{argument => ::doc_id}; # 4 test purposes
 
 
 sub filter_files() {
@@ -254,12 +358,20 @@ sub file_revisions_get  {
 		shave	=> {type => SCALAR, optional => 1},
 		debug 	=> {type => SCALAR, optional => 1, default => undef},
 		});
+	
 	my @fields = qw/kind revisions/;
+	
+	my $request_string = prepare_request(
+		api => 'files/'.$arg{file_id}.'/revisions/', 
+		flags	=> undef,
+		fields	=> \@fields,
+		);
+
 	my $response = call_api(
-			api => 'files/'.$arg{file_id}.'/revisions/', 
-			flags	=> undef,
-			fields	=> \@fields,
-			);
+		request => $request_string,
+		#debug => \$arg{debug},
+		);
+
 	foreach my $revision ( @{ $response->{revisions} } ) {
 		$revision = shave($revision, qw/id kind lastModifyingUser 
 									 mimeType modifiedTime/);
@@ -267,8 +379,9 @@ sub file_revisions_get  {
 		#$revision->{lastModifyingUser} = shave( $revision->{lastModifyingUser}, 
 		#										qw/displayName kind/);
 	}
-	print Dumper $response;
+	return $response;
 }
+
 
 
 
@@ -293,16 +406,21 @@ sub file_revision_get {# ( $file_id, $revision_id )
 		shave	=> {type => SCALAR, optional => 1},
 		debug 	=> {type => SCALAR, optional => 1, default => undef},
 		});
-	my $response = call_api(
+	my $request_string = prepare_request(
 		api 	=> 'files/'.$arg{file_id}.'/revisions/'.$arg{revision_id}, 
 		flags	=> undef,
 		fields	=> \@dfields,
 		);
-	# only proto'ed. need 2 implement the holy patch semantics as in gdrive-merge to harm ATL
-	# https://developers.google.com/drive/v3/reference/changes/getStartPageToken & ibid., overall
+	my $response = call_api(
+		request => $request_string,
+		#debug => \$arg{debug},
+		);
+	# only proto'ed. do stuff w/ patch semantics
 	print Dumper $response if defined $arg{debug};
 	return $response;
 }
+
+#file_revision_get(file_id=>$file_id, revision_id=>4069);
 
 
 # Implement collector of LMUT's (possibly, w/ originated-from-UID)
@@ -318,51 +436,63 @@ sub file_revision_get {# ( $file_id, $revision_id )
 ### { "kind": "drive#commentList", "comments": [ 
 #	  { "kind": "drive#comment", commentId, times, author, "replies": [ 
 #			{"kind": "drive#reply" is_derived_from: "drive#comment"+@action } ]
-sub file_comments_get { # mandatory requirements -- need 2 set 'can comment'-permission for each of srcs
+sub file_comments_get { 
+# mandatory requirements -- need 2 set 'can comment'-permission for each of srcs
 	my %arg = validate( @_, {
 		target	=> {type => HASHREF, optional => 1},
 		file_id	=> {type => SCALAR},
 		page_size => {type => SCALAR, optional => 1, default => 10},
 		fields	=> {type => ARRAYREF, optional => 1},
-		fkeys	=> {type => ARRAYREF, optional => 1}, # critical
+		fkeys	=> {type => ARRAYREF, optional => 1},
 		flags	=> {type => ARRAYREF, optional => 1}, 
 		shave	=> {type => SCALAR, optional => 1},
 		debug 	=> {type => SCALAR, optional => 1, default => undef},
 		});
-	my @flags = ['includeDeleted=true', "pageSize=".$arg{page_size}, ''];
-	my @fields = ['kind', 'comments', 'nextPageToken'];
+	my @flags = ['includeDeleted=true', "pageSize=".$arg{page_size}];
+	my @fields = ['kind', 'comments', 'nextPageToken',]; # sensitive to undef
 	my @comments = ();
 	my $next_page = undef;
 	my $result;
 	my ($nor,$noc) = 0; # overall number of requests
+	my $time0 = time;
 	do { # too flacky, may use CSP?
-		$result = call_api( # fetch w/ returning the nxt-bearer
+		my $request_string = prepare_request(
 			api		=> 'files/'.$arg{file_id}.'/comments',
 			flags	=> @flags,
 			fields	=> @fields,
-			debug	=> 'request',
-		);
-		print Dumper $result if $arg{debug};
+			debug	=> "request",
+			);
+		print "\n\n$request_string\n\n";
+		$result = call_api( # fetch w/ the bearer
+			request => $request_string,
+			debug => $arg{debug},
+			);
+		print eDumper $result if $arg{debug};
 		$next_page = $result->{nextPageToken} if $result;
-		pop $flags[0]; push $flags[0], "pageToken=$next_page" if $next_page;
-		$nor++; # for debug purposes
-		foreach my $comment (@{ $result->{comments} }) { # lazy to use map-magic
-			#	$comment = shave(\$comment, qw/kind id createdTime modifiedTime author 
-			#					deleted resolved replies/);
-			#	# shave author and comments hashes?
-			#	update_fields(\%$comment, \&time2ut, \@{qw/createdTime modifiedTime/});
-			#}
+		$flags[0][-1] = "pageToken=$next_page" if $next_page;
+		$nor++;
+		foreach my $comment (@{ $result->{comments} }) {
+			# shave comment
 			push @comments, $comment;
-			# stir the entire stuff for distilled hash
 		}
-		print "\t\tnoc in resp: ", scalar @{$result->{comments}}, "\n" if $arg{debug};
+		print "\t\tnoc in resp: ", scalar @{$result->{comments}}, "\n";
 	} while ($next_page);# || $result->{nextPageToken}
-	print "\tnoc in comments: ", scalar @comments, "\n" if $arg{debug};
+	print "\tnoc in comments: ", scalar @comments, "\n";
+	my $tdiff = sprintf "%03d msec", (time - $time0)*1000 ;
+	print "\toverall took $tdiff\n" if $arg{debug};
+	#foreach my $comment ( @$comments ) {
+	#	$comment = shave(\$comment, qw/kind id createdTime modifiedTime author 
+	#								  deleted resolved replies/);
+	#	# shave author and comments hashes?
+	#	update_fields(\%$comment, \&time2ut, \@{qw/createdTime modifiedTime/});
+	#}
 	#print Dumper {@comments} if ($arg{debug} eq 'dump all');
 	#return \%comments;
 }
+
 my $test_document = '' unless $glob{argument => ::doc_id}; # 4 test purposes
 file_comments_get(file_id => $test_document, page_size => 2);
+print Dumper {%counter};
 
 
 
